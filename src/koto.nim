4da5ec34cd137ab32aad8e22cf1792eb1118a0d1
t-sin <shinichi.tanaka45@gmail.com>
2018-10-23 00:54:22 +0900

    Basic operation with Portaudio

--- src/koto.nim
+++ src/koto.nim
@@ -1,7 +1,29 @@
+import portaudio as pa
+
 import kotopkg.main
+
+
+# fundamental proc and it should be replaced
+proc procBuffer(inBuf, outBuf: pointer,
+                framesPerBuf: culong,
+                timeInfo: ptr pa.TStreamCallbackTimeInfo,
+                stateusFlags: pa.TStreamCallbackFlags,
+                userData: pointer): cint {.cdecl.} =
+  return 0
+
 
 when isMainModule:
   echo "hi, Koto!"
 
   init()
-  term()
+  var s = start(procBuffer)
+  try:
+    while true:
+      pa.Sleep(1)
+
+  except KeyboardInterruptError:
+    echo "Keyboard Interrupted"
+    term(s)
+    quit 0
+
+  term(s)
--- src/kotopkg/main.nim
+++ src/kotopkg/main.nim
@@ -1,6 +1,45 @@
+import portaudio as pa
+
+
+type KeyboardInterruptError* = object of Exception
+proc handleError() {.noconv.} =
+  raise newException(KeyboardInterruptError, "Keyboard Interrupt")
+
+setControlCHook(handleError)
+
+
+proc initPA(): void =
+  discard pa.Initialize()
+
+proc termPA(stream: PStream): void =
+  discard pa.StopStream(stream)
+  discard pa.CloseStream(stream)
+  discard pa.Terminate()
+
+proc startPA(procPaBuffer: proc): PStream =
+  var stream: PStream
+
+  discard pa.OpenDefaultStream(
+    cast[PStream](stream.addr),
+    numInputChannels = 0,
+    numOutputChannels = 2,
+    sampleFormat = pa.TSampleFormat.sfFloat32,
+    sampleRate = cdouble(44100),
+    framesPerBuffer = culong(1024),
+    streamCallback = procPaBuffer,
+    userData = nil)
+
+  discard pa.StartStream(stream)
+  return stream
+
 
 proc init*(): void =
   echo "----initializing----"
+  initPA()
 
-proc term*(): void =
+proc start*(procPaBuffer: proc): PStream =
+  return startPA(procPaBuffer)
+
+proc term*(stream: PStream): void =
   echo "----terminating----"
+  termPA(stream)
